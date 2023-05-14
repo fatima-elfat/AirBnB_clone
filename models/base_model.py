98f1f82527c22e3e7fa96f60c70e8e12b44f9222
2oothpick <kobbyboateng14@gmail.com>
2023-05-14 07:43:58 +0000

    updated base_model module

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,35 @@
+from uuid import uuid4
+from datetime import datetime
+
+
 class BaseModel:
     """
     Defines all common attributes/methods for other classes
     """
-    
+    id = str(uuid4())
+    created_at = datetime.now()
+    updated_at = datetime.now()
+
+
+    def __str__(self):
+        """
+        Returns a string representation of the class
+        """
+        return f"{self.__class__.__name__} {self.id} {str(self.__dict__)}"
+
+    def save(self):
+        """ Updates the public instance attribute
+            updated_at
+        """
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """
+        Returns a dictionary containing all keys/values of __dict__
+        of the instance
+        """
+        diction = {
+            "__class__" : self.__class.__name__,
+            "created_at": self.created_at.isoformat(),
+            "updated_at": self.updated_at.isoformat()
+        }
