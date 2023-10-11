ddc6b331be12936d741c22c85d03a209f92bcdf2
2oothpick <kobbyboateng14@gmail.com>
2023-10-11 23:27:04 +0000

    fix methods in FileStorage class

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -13,27 +13,27 @@ class FileStorage():
     """Serializes instances to a JSON file
         and deserializes JSON file to instances
     """
-    __file_path = "" # path to JSON file
+    __file_path = "file.json" # path to JSON file
     __objects = {} # will store objects(<class name>.id)
 
     def all(self):
         """Returns the dictionary __objects"""
-        return FileStorage.__objects 
+        return self.__objects 
     
     def new(self, obj):
-        """Populates __objects with <obj class name> as key"""
-        FileStorage.__objects[obj.id] = obj
+        """Populates __objects with <obj class name>.obj as key"""
+        self.__objects[obj.__class__.__name__+ '.' + obj.id] = obj
 
     def save(self):
         """Serializes __objects to the JSON file"""
-        with open(FileStorage.__file_path, 'w') as file:
-            json.dump(FileStorage.__objects, file)
+        with open(self.__file_path, 'w') as file:
+            json.dump(self.__objects, file)
     
     def reload(self):
         """Deserializes the JSON file to __objects"""
         try:
-            if os.path.isfile(FileStorage.__file_path):
-                with open(FileStorage.__file_path) as file:
+            if os.path.isfile(self.__file_path):
+                with open(self.__file_path) as file:
                     return json.load(file)
         except:
             pass
