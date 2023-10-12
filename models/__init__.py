0a81f867e0ef78b82319c97caf406ebd9d3bf56d
2oothpick <kobbyboateng14@gmail.com>
2023-10-12 23:00:45 +0000

    Add shebang to models/__init__

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,6 @@
+#!/usr/bin/python3
 from models.base_model import BaseModel
-from engine.file_storage import FileStorage
+from models.engine.file_storage import FileStorage
 
 
 storage = FileStorage()
