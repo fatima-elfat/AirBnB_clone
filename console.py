b36463c37a30963a89ed4582bd4cbdbd2fb35ba4
2oothpick <kobbyboateng14@gmail.com>
2023-10-14 23:13:12 +0000

    Add create method
    Creates a new instacance of BaseModel if arg is BaseModel

--- console.py
+++ console.py
@@ -3,12 +3,26 @@
 """Entry point of the command interpreter"""
 
 import cmd
+from models.base_model import BaseModel
+from models import storage
 
 
 class HBNBCommand(cmd.Cmd):
-    """HBNB command interpreter"""
-
+    """HBNB class"""
     prompt = "(hbnb)"
+    __classes = ["BaseModel"] # list of existing classes
+
+    def do_create(self, arg):
+        """Creates a new instance of BaseModel"""
+        if arg:
+            if arg in HBNBCommand.__classes:
+                print(BaseModel().id)
+                storage.save()
+            else: # class name not in __classes
+                print("class doesn't exist")
+        else:
+            print("class name missing")
+        
     def do_quit(self, arg):
         """Exit the interpreter"""
         return True
