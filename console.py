2a16e892c06cc7c24d63269baca9f1177f279fbf
2oothpick <kobbyboateng14@gmail.com>
2023-10-15 00:44:54 +0000

    Add do_show method for console

--- console.py
+++ console.py
@@ -22,7 +22,33 @@ class HBNBCommand(cmd.Cmd):
                 print("class doesn't exist")
         else:
             print("class name missing")
-        
+    
+    def do_show(self, arg):
+        """Prints the string representation of an
+        instance by its class name and id
+        """
+        if arg:
+            objects = storage.all()
+            list_args = arg.split()
+            if list_args[0]:
+                if list_args[0] in HBNBCommand.__classes:
+                    if list_args[1]:
+                        class_id = f"{list_args[0]}.{list_args[1]}"
+                        if class_id in objects:
+                            print(objects[class_id])
+                        else: 
+                            print("no instance found")
+                    else:
+                        print("instance id missing")
+                else:
+                    print("class doesn't exist")
+            else:
+                print("class name missing")
+        else:
+            print("class name missing")
+
+    
+
     def do_quit(self, arg):
         """Exit the interpreter"""
         return True
