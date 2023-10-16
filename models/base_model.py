26df0ffdcfa45ace1f37d889c3243f4120b1b3dc
fatima-elfat <ffzelfatehy@gmail.com>
2023-10-16 01:08:17 +0100

    fix str function in base model

--- models/base_model.py
+++ models/base_model.py
@@ -36,9 +36,12 @@ class BaseModel:
 
     def __str__(self):
         """
-        Returns a string representation of the class
+        Return str of information of BaseModel.
         """
-        return f"{[self.__class__.__name__]} ({self.id}) {str(self.__dict__)}"
+        return ('[{}] ({}) {}'.format(
+            self.__class__.__name__,
+            self.id, self.__dict__
+            ))
 
     def save(self):
         """ Updates the public instance attribute
